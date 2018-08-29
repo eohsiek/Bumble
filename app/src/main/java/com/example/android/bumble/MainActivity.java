c6bdb0b321a4eda8ba7275978a867da85a898c31
eohsiek <eohsiek@gmail.com>
2018-08-28 22:04:32 -0600

    Alter Bottom Nav

--- app/src/main/java/com/example/android/bumble/MainActivity.java
+++ app/src/main/java/com/example/android/bumble/MainActivity.java
@@ -20,11 +20,11 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_favorites:
+                    mTextMessage.setText(R.string.title_favorites);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_suggest);
                     return true;
             }
             return false;
