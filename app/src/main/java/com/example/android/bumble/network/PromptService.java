c84f4d1eecc34d47859b61b9a2c3754bda94ff3d
eohsiek <eohsiek@gmail.com>
2018-10-24 19:37:54 -0600

    Remove widget

--- app/src/main/java/com/example/android/bumble/network/PromptService.java
+++ app/src/main/java/com/example/android/bumble/network/PromptService.java
@@ -1,14 +1,22 @@
 //thanks to https://code.tutsplus.com/tutorials/getting-started-with-retrofit-2--cms-27792 as a starting point for the code below
 package com.example.android.bumble.network;
 
+import android.os.Build;
+
+import com.example.android.bumble.BuildConfig;
 import com.example.android.bumble.network.pojo.PromptResponse;
 import com.example.android.bumble.network.pojo.SuggestionResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
+
+
 public interface PromptService {
+
+    @Headers("x-api-key: " + BuildConfig.apikey)
 
     @GET("?")
     Call<PromptResponse> getPrompt(
